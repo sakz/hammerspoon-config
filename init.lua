89c2a3dd9657f62677926d72534d93179a38b402
jiazhao <fayecy@qq.com>
2019-07-01 15:16:08 +0800

    for me, just two

--- init.lua
+++ init.lua
@@ -1,18 +1,3 @@
 print('==================================================')
-require "headphone.headphone"
-require "hotkey.hotkey"
 require "ime.ime"
-require "reload.reload"
-require "usb.usb"
-require "wifi.wifi"
-require "window.window"
-require "clipboard.clipboard"
-require "statuslets.statuslets"
-require "volume.volume"
 require "weather.weather"
-require "speaker.speaker"
-
--- Private use
-if (hs.host.localizedName() == 'kaboom的MacBook Pro') then
-	require("autoscript.autoscript")
-end
